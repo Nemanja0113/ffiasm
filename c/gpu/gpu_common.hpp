f534d5975bd1845179b47c4c4131ffab1e7eb1fe
dlee53931 <dlee53931@gmail.com>
2025-09-05 04:19:30 +0900

    fix warning

--- c/gpu/gpu_common.hpp
+++ c/gpu/gpu_common.hpp
@@ -28,21 +28,5 @@ struct G1Point {
 extern __constant__ uint64_t Fq_prime[4];
 extern __constant__ uint64_t Fq_np;
 
-// Field arithmetic function declarations (defined in gpu_msm_kernels.cu)
-// Note: These are declared without __device__ here since they're defined in a different .cu file
-bool fq_is_zero(const FqElement* a);
-void fq_zero(FqElement* result);
-void fq_one(FqElement* result);
-void fq_copy(FqElement* result, const FqElement* a);
-void fq_add(FqElement* result, const FqElement* a, const FqElement* b);
-void fq_sub(FqElement* result, const FqElement* a, const FqElement* b);
-void fq_mul(FqElement* result, const FqElement* a, const FqElement* b);
-
-// Point operation function declarations (defined in gpu_msm_kernels.cu)
-// Note: These are declared without __device__ here since they're defined in a different .cu file
-bool point_is_zero(const G1Point* a);
-void point_zero(G1Point* result);
-void point_copy(G1Point* result, const G1Point* src);
-void point_add(G1Point* result, const G1Point* a, const G1Point* b);
-void point_add(G1Point* result, const G1Point* a, const G1PointAffine* b);
-void point_sub(G1Point* result, const G1Point* a, const G1PointAffine* b);
+// Field arithmetic and point operation functions are defined in each .cu file as needed
+// No function declarations here to avoid redeclaration warnings
